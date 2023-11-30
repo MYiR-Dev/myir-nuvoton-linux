5dd1f2e3b80d21c13403a90c9f7403bcfcdc6fbf
crack.xue <crack.xue@myir.cn>
2023-11-30 02:32:56 +0000

    FEAT: add reset-gpio for gmac
    FEAT: change gmac phyaddr
    FEAT: change reset-gpios to high
    FEAT: add the phy reset

--- drivers/net/ethernet/stmicro/stmmac/dwmac-ma35d1.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-ma35d1.c
@@ -12,6 +12,8 @@
 #include <linux/of_net.h>
 #include <linux/module.h>
 #include <linux/of_device.h>
+#include <linux/of_mdio.h>
+#include <linux/of_gpio.h>
 #include <linux/platform_device.h>
 #include <linux/mfd/syscon.h>
 #include <linux/regmap.h>
@@ -33,6 +35,62 @@ struct nvt_priv_data {
 	int tx_delay;
 	int rx_delay;
 };
+
+static int fec_reset_phy(struct platform_device *pdev)
+{
+	int err, phy_reset;
+	bool active_high = false;
+	int msec = 1, phy_post_delay = 0;
+	struct device_node *np = pdev->dev.of_node;
+
+	if (!np)
+		return 0;
+
+	err = of_property_read_u32(np, "phy-reset-duration", &msec);
+	/* A sane reset duration should not be longer than 1s */
+	if (!err && msec > 1000)
+		msec = 1;
+
+	phy_reset = of_get_named_gpio(np, "phy-reset-gpios", 0);
+	if (phy_reset == -EPROBE_DEFER)
+		return phy_reset;
+	else if (!gpio_is_valid(phy_reset))
+		return 0;
+
+	err = of_property_read_u32(np, "phy-reset-post-delay", &phy_post_delay);
+	/* valid reset duration should be less than 1s */
+	if (!err && phy_post_delay > 1000)
+		return -EINVAL;
+
+	active_high = of_property_read_bool(np, "phy-reset-active-high");
+
+	err = devm_gpio_request_one(&pdev->dev, phy_reset,
+								active_high ? GPIOF_OUT_INIT_HIGH : GPIOF_OUT_INIT_LOW,
+								"phy-reset");
+	if (err)
+	{
+		dev_err(&pdev->dev, "failed to get phy-reset-gpios: %d\n", err);
+		return err;
+	}
+
+	gpio_set_value_cansleep(phy_reset, !active_high);
+	if (msec > 20)
+		msleep(msec);
+	else
+		usleep_range(msec * 1000, msec * 1000 + 1000);
+	gpio_set_value_cansleep(phy_reset, active_high);
+
+	if (!phy_post_delay)
+		return 0;
+
+	if (phy_post_delay > 20)
+		msleep(phy_post_delay);
+	else
+		usleep_range(phy_post_delay * 1000,
+					 phy_post_delay * 1000 + 1000);
+	gpio_set_value_cansleep(phy_reset, !active_high);
+	return 0;
+}
 
 static struct nvt_priv_data *nvt_gmac_setup(struct platform_device *pdev,
 					    struct plat_stmmacenet_data *plat)
@@ -127,6 +185,10 @@ static int nvt_gmac_probe(struct platform_device *pdev)
 	plat_dat->has_gmac = 1;
 	plat_dat->pmt = 1;
 
+	ret = fec_reset_phy(pdev);
+	if (ret)
+		printk("phy_reset fail\n");
+		
 	plat_dat->bsp_priv = nvt_gmac_setup(pdev, plat_dat);
 	if (IS_ERR(plat_dat->bsp_priv)) {
 		ret = PTR_ERR(plat_dat->bsp_priv);
